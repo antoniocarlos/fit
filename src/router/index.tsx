f5a0bf40ac85ab3bf51c76de4e618abec26c35c4
Antonio Thiele <athiele@multiplan.com.br>
2022-11-12 21:47:28 -0300

    setup project

---
+++ src/router/index.tsx
@@ -0,0 +1,18 @@
+
+
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('../pages/Home'));
+
+const Router = () => {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Suspense>
+  )
+}
+
+export default Router
